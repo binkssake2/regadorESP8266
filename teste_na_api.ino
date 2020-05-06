d5357e67fcf29f513ab3bc7e4c3b3c9ede978243
binkssake2 <45924236+binkssake2@users.noreply.github.com>
2020-05-06 17:39:37 -0300

    modify:  LCD still bugs, but funciona

--- teste_na_api.ino
+++ teste_na_api.ino
@@ -5,14 +5,15 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 #include <vector>
-#include <Wire.h>       //I2C library
-#include <RtcDS3231.h>  //RTC library
+#include <Wire.h>
+#include <DS3232RTC.h>
 #include <LiquidCrystal.h>
-
-const int RS = D3, EN = D5, d4 = D6, d5 = D7, d6 = D8, d7 = D9;
+#define BUTTON_RESET_WIFI D3
+const int rele = D5;
+const int RS = D0, EN = D4, d4 = D6, d5 = D7, d6 = D8, d7 = D9;
 LiquidCrystal lcd(RS, EN, d4, d5, d6, d7);
 
-RtcDS3231<TwoWire> rtcObject(Wire);
+DS3232RTC RTC;
 
 DynamicJsonDocument db(11424);
 
@@ -21,7 +22,72 @@ WiFiServer server(80);
 unsigned long millis_db = 0;
 unsigned long millis_rtc = 0;
 unsigned long millis_db_display = 0;
+unsigned long millis_lcd_clear = 0;
 int k = 0;
+
+ICACHE_RAM_ATTR void reset_wifi() {
+  WiFiManager wifiManager;
+  wifiManager.resetSettings();
+}
+
+void precisa0(int digito) {
+  if (digito < 10) {
+    lcd.print('0');
+  }
+}
+
+void reconnectWIFI() {
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Não foi possivel conectar ao servidor WEB ou banco de dados");
+    lcd.setCursor(0, 0);
+    lcd.print("Caiu, try recon");
+    WiFi.reconnect();
+    delay(3000);
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("Reconectou");
+      lcd.setCursor(0, 0);
+      lcd.print("Reconectado!!!!");
+      delay(3000);
+    }
+  }
+}
+
+
+void lcd_print_hora(int hora, int minuto, int segundo) {
+  //LCD PRINT DA HORA
+  lcd.setCursor(0, 1);
+  lcd.print("Hora:");
+  precisa0(hora);
+  lcd.print(hora);
+  lcd.print(":");
+  precisa0(minuto);
+  lcd.print(minuto);
+  lcd.print(":");
+  precisa0(segundo);
+  lcd.print(segundo);
+  delay(1000);
+}
+
+int lcd_print_db(int len_db, DynamicJsonDocument db, int k) {
+  lcd.setCursor(0, 0);
+  lcd.print("I ");
+  precisa0(int(db[k][0][0]));
+  lcd.print(int(db[k][0][0]));
+  lcd.print(":");
+  precisa0(int(db[k][0][1]));
+  lcd.print(int(db[k][0][1]));
+  lcd.print(" ");
+  lcd.print("D ");
+  precisa0(int(db[k][1][0]));
+  lcd.print(int(db[k][1][0]));
+  lcd.print(":");
+  precisa0(int(db[k][1][1]));
+  lcd.print(int(db[k][1][1]));
+  millis_db_display = millis();
+  return k + 1;
+}
+
 
 void add_att_RTC() {
   if (WiFi.status() == WL_CONNECTED) { //checa conexão wifi
@@ -48,10 +114,23 @@ void add_att_RTC() {
       int mes = 10 * (int(infos_date[5]) - 48) + (int(infos_date[6]) - 48);
       int dia = 10 * (int(infos_date[8]) - 48) + (int(infos_date[9]) - 48);
       int hora = 10 * (int(infos_date[11]) - 48) + (int(infos_date[12]) - 48);
-      int minuto = 10 * (int(infos_date[14]) - 48) + ((infos_date[15]) - 48);
+      int minuto = 10 * (int(infos_date[14]) - 48) + (int(infos_date[15]) - 48);
+      int segundo = 10 * (int(infos_date[17] - 48)) + (int(infos_date[18]) - 48);
 
-      RtcDateTime currentTime = RtcDateTime(ano, mes, dia, hora, minuto, 0); 
-      rtcObject.SetDateTime(currentTime);
+      setTime(hora, minuto, segundo, dia, mes, ano);
+      RTC.set(now());
+      lcd.setCursor(0, 0);
+      lcd.print("TEMPO NA API");
+      lcd.setCursor(0, 1);
+      lcd.print("Hora:");
+      lcd.print(hour());
+      lcd.print(":");
+      lcd.print(minute());
+      lcd.print(":");
+      lcd.print(second());
+      lcd.print("   ");
+      delay(5000);
+      lcd.clear();
 
     }
 
@@ -87,115 +166,131 @@ DynamicJsonDocument add_att_db(DynamicJsonDocument db) {
   return db;
 }
 
-
-
 void setup() {
-  pinMode(D4, OUTPUT);
-  digitalWrite(D4, LOW);
+  pinMode(BUTTON_RESET_WIFI, INPUT_PULLUP);
+
+  digitalWrite(rele, LOW);
+  pinMode(rele, OUTPUT);
+
   Wire.begin();
-  rtcObject.Begin();
-  
+
   Serial.begin(115200);
+
+  setSyncProvider(RTC.get);   // the function to get the time from the RTC
+  if (timeStatus() != timeSet)
+    Serial.println("Unable to sync with the RTC");
+  else
+    Serial.println("RTC has set the system time");
 
   lcd.begin(16, 2);
   lcd.print("Config Wi-Fi...");
 
   WiFiManager wifiManager;
 
-  wifiManager.autoConnect("Regador de Fror");
+  wifiManager.setTimeout(120);
+
+  if (!wifiManager.autoConnect("Regador de Flor")) {
+    Serial.println("Falhou ao conectar e deu timeout");
+    delay(3000);
+    ESP.reset();
+    delay(5000);
+  }
 
   Serial.println("conectouu :)");
   server.begin();
+
   add_att_RTC();
+
+  delay(2000);
+
   db = add_att_db(db);
-  
-  lcd.clear();
-  lcd.print("Conectado!");
-  
+  lcd.setCursor(0, 0);
+  lcd.print("!!!Conectado!!!");
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
+
   Serial.println("setup over");
   delay(3000);
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON_RESET_WIFI), reset_wifi, FALLING);
 
 }
 
 void loop() {
-  RtcDateTime currentTime = rtcObject.GetDateTime();
+
+  reconnectWIFI();
+
   int len_db = db.size();
 
-  //SERIAL DEBUG
-  Serial.print(currentTime.Hour());
-  Serial.print(":");
-  Serial.print(currentTime.Minute());
-  Serial.println(" ");
-  delay(1000);  
+  lcd_print_hora(hour(), minute(), second());
 
-  //LCD PRINT DA HORA
-  lcd.setCursor(0, 1);
-  lcd.print("Hora:");
-  lcd.print(currentTime.Hour());
-  lcd.print(":");
-  lcd.print(currentTime.Minute());
-  lcd.print(":");
-  lcd.print(currentTime.Second());
-
-  //LCD PRINT DB
-  
   if (millis() - millis_db_display >= 5000) {
-    lcd.clear();
-    lcd.print("I ");
-    lcd.print(int(db[k][0][0]));
-    lcd.print(":");
-    lcd.print(int(db[k][0][1]));
-    lcd.print(" ");
-    lcd.print("Dur ");
-    lcd.print(int(db[k][1][0]));
-    lcd.print(":");
-    lcd.print(int(db[k][1][1]));
-    millis_db_display = millis();
-    k++;
+    k = lcd_print_db(len_db, db, k);
   }
-    if(k == len_db) {
-      k = 0;
-    }
-  
+  if (k == len_db) {
+    k = 0;
+  }
 
-
-
-  
   ///////////att tempo RTC de 24 em 24 horas e banco de dados de 5 em 5 min///////////
-  if (millis() - millis_rtc >= 86400000) { //24 horas    = 86400000, 1 hora = 3600000
+  if (millis() - millis_rtc >= 300000) { //24 horas    = 86400000, 1 hora = 3600000
     add_att_RTC();
     millis_rtc = millis();
+    //lcd.clear();
+    //lcd.print("att RTC         ");
+    delay(2000);
     Serial.println("att rtc");
+    lcd.clear();
   }
-  
-   if (millis() - millis_db >= 300000) { //24 horas    = 86400000, 1 hora = 3600000
+
+  if (millis() - millis_db >= 120000) { //24 horas    = 86400000, 1 hora = 3600000, 300000 5 min
     db = add_att_db(db);
     millis_db = millis();
+    lcd.setCursor(0, 0);
+    lcd.print("att DB          ");
+    delay(3000);
     Serial.println("att db");
+    k = 0;
+    len_db = db.size();
   }
 
-  
-  
+
+
 
   //ROTINA DE TEMPO DO REGADOR PARA FAZER O PROCESSO PROPRIAMENTE DITO
-  for (int i = 0; i < len_db; i++) {
-   if ((int(db[i][0][0])) == currentTime.Hour()) { //*******************horas
-      if ((int(db[i][0][1])) == currentTime.Minute()) { //****************minutos
-        Serial.println("ENTROU");
-        
-        int tempo_segundos = 60 * int((db[i][1][1])) + 3600 * int ((db[i][1][0]));
-        Serial.println(tempo_segundos);
-        int millis_rega = millis();
-        
-        while (millis() - millis_rega < tempo_segundos * 1000) {
-          digitalWrite(D4, HIGH);
-          Serial.println(millis() - millis_rega);
-          Serial.println(digitalRead(D4));
-          delay(1000);
+  if (len_db > 0) {
+    for (int i = 0; i < len_db; i++) {
+      if ((int(db[i][0][0])) == hour()) { //*******************horas
+        if ((int(db[i][0][1])) == minute()) { //****************minutos
+          Serial.println("ENTROU");
+
+          int tempo_segundos = 60 * int((db[i][1][1])) + 3600 * int ((db[i][1][0]));
+
+          //lcd.clear();
+          lcd.print("Rega por ");
+          lcd.print(tempo_segundos);
+          lcd.print("seg");
+          lcd.setCursor(0, 1);
+          lcd.print("Inicio:");
+          lcd.print(int(db[i][0][0]));
+          lcd.print(":");
+          lcd.print(int(db[i][0][1]));
+          lcd.print("     ");
+
+
+          Serial.println(tempo_segundos);
+          int millis_rega = millis();
+
+          while (millis() - millis_rega < tempo_segundos * 1000) {
+            digitalWrite(rele, HIGH);
+            Serial.println(millis() - millis_rega);
+            Serial.println(digitalRead(rele));
+            delay(1000);
+          }
+          Serial.println("Saiu");
+          digitalWrite(rele, LOW);
+          millis_rega = millis();
+          i = 0;
         }
-        Serial.println("Saiu");
-        digitalWrite(D4,LOW);
-        millis_rega = millis();
       }
     }
   }
